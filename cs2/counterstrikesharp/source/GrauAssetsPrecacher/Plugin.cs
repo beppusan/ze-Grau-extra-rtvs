d8741a034fe663dce252e18c3cbbe188bf6e225f
beppu-san <hayato1001_f@outlook.jp>
2024-12-08 11:56:07 +0900

    Implement rtv-FyS for CS2

---
+++ cs2/counterstrikesharp/source/GrauAssetsPrecacher/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace GrauAssetsPrecacher;
+
+public class GrauAssetsPrecacher : BasePlugin
+{
+    public override string ModuleAuthor => "Beppu";
+    public override string ModuleName => "GrauAssetsPrecacher";
+	public override string ModuleDescription => "Precaches assets for ZE Grau";
+    public override string ModuleVersion => "1.0.0";
+
+    public override void Load(bool hotReload)
+    {
+        RegisterListener<Listeners.OnServerPrecacheResources>((ResourceManifest manifest) =>
+		{
+			manifest.AddResource("soundevents/soundevents_ze_grau.vsndevts");
+		});
+    }
+}
