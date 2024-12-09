3c5a3060378851b061aa84a934fb6e17e00c638d
beppu-san <hayato1001_f@outlook.jp>
2024-12-09 13:13:56 +0900

    Tabify indentation

--- cs2/counterstrikesharp/source/GrauAssetsPrecacher/Plugin.cs
+++ cs2/counterstrikesharp/source/GrauAssetsPrecacher/Plugin.cs
@@ -5,16 +5,16 @@ namespace GrauAssetsPrecacher;
 
 public class GrauAssetsPrecacher : BasePlugin
 {
-    public override string ModuleAuthor => "Beppu";
-    public override string ModuleName => "GrauAssetsPrecacher";
+	public override string ModuleAuthor => "Beppu";
+	public override string ModuleName => "GrauAssetsPrecacher";
 	public override string ModuleDescription => "Precaches assets for ZE Grau";
-    public override string ModuleVersion => "1.0.0";
+	public override string ModuleVersion => "1.0.0";
 
-    public override void Load(bool hotReload)
-    {
-        RegisterListener<Listeners.OnServerPrecacheResources>((ResourceManifest manifest) =>
+	public override void Load(bool hotReload)
+	{
+		RegisterListener<Listeners.OnServerPrecacheResources>((ResourceManifest manifest) =>
 		{
 			manifest.AddResource("soundevents/soundevents_ze_grau.vsndevts");
 		});
-    }
+	}
 }
